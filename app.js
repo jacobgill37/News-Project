b49edff59e4a9cf6d4b2fffaa5233a05fff700bd
jacobgill37 <jacobgill37@hotmail.com>
2022-11-14 16:00:42 +0000

    5. GET by id

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticleById);
 
 app.use((err, req, res, next) => {
-  console.log(err);
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid query datatype" });
   } else {
