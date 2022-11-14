cdc274d9e307100701edee3b34d385deac4b727e
jacobgill37 <jacobgill37@hotmail.com>
2022-11-14 15:45:46 +0000

    second before merge

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -25,38 +25,3 @@ describe("GET /api/topics", () => {
       });
   });
 });
-
-describe("GET /api/articles/:article_id", () => {
-  test("200: should respond with the given article", () => {
-    return request(app)
-      .get("/api/articles/1")
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.article).toMatchObject({
-          author: expect.any(String),
-          title: expect.any(String),
-          article_id: expect.any(Number),
-          topic: expect.any(String),
-          created_at: expect.any(String),
-          votes: expect.any(Number),
-          body: expect.any(String),
-        });
-      });
-  });
-  test("404: Should respond with 404 if passed a non-existent id", () => {
-    return request(app)
-      .get("/api/articles/1000")
-      .expect(404)
-      .then(({ body }) => {
-        expect(body.msg).toBe("Article not found");
-      });
-  });
-  test("400: Should respond with 400 if passed a wrong datatype", () => {
-    return request(app)
-      .get("/api/articles/notANumber")
-      .expect(400)
-      .then(({ body }) => {
-        expect(body.msg).toBe("Invalid query datatype");
-      });
-  });
-});
--- app.js
+++ app.js
@@ -7,6 +7,4 @@ app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
-app.get("api/articles/:article_id", getArticleById);
-
 module.exports = app;
