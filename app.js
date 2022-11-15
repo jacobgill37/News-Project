0193a616e47d6e3ee4ae7b411f96e877ca87228c
jacobgill37 <jacobgill37@hotmail.com>
2022-11-15 10:53:31 +0000

    add psql error handling

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.post("/api/articles/:article_id/comments", postComment);
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid query datatype" });
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "Article not found" });
   } else {
     next(err);
   }
