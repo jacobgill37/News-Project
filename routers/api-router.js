5a16c866f2d13a1e6f6f980df8e0eeb32bbb70b7
jacobgill37 <jacobgill37@hotmail.com>
2022-11-17 13:41:07 +0000

    add seed endpoint

--- routers/api-router.js
+++ routers/api-router.js
@@ -13,4 +13,8 @@ apiRouter.use("/users", usersRouter);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
 
+apiRouter.get("/seed", () => {
+  require("../db/seeds/run-seed.js");
+});
+
 module.exports = apiRouter;
