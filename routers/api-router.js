3526107e17de89f2c8e1819d1257b6930a331a7f
jacobgill37 <jacobgill37@hotmail.com>
2022-11-17 14:37:59 +0000

    add db url and env to seed res

--- routers/api-router.js
+++ routers/api-router.js
@@ -18,7 +18,11 @@ apiRouter.get("/seed", (req, res) => {
   process.env.DATABASE_URL =
     "postgres://sjbnfswb:FhdEp8sTmI_Smk3MR_xxTgwtL2J83pNH@lucky.db.elephantsql.com/sjbnfswb";
   require("../db/seeds/run-seed.js");
-  res.send({ msg: "Production database seeded" });
+  res.send({
+    msg: "Production database seeded",
+    URL: process.env.DATABASE_URL,
+    env: process.env.NODE_ENV,
+  });
 });
 
 module.exports = apiRouter;
